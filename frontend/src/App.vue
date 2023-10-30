9af20e543bcbe0a711c93a87abc9c3cf30b1e713
Lucas Remigio <2211034@my.ipleiria.pt>
2023-10-30 14:20:46 +0000

    toques na tabela

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,34 +1,34 @@
 <template>
     <div class="container mt-4">
-      <h1 class="text-center">vCard Details</h1>
-      <div v-if="vcards.length > 0">
-        <table class="table table-striped table-bordered">
-          <thead class="thead-dark">
-            <tr>
-              <th>Name</th>
-              <th>Email</th>
-              <th>Phone Number</th>
-              <th>Balance</th>
-              <th>Max Debit</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-for="vcard in vcards" :key="vcard.phone_number">
-              <td>{{ vcard.name }}</td>
-              <td>{{ vcard.email }}</td>
-              <td>{{ vcard.phone_number }}</td>
-              <td>{{ vcard.balance }}</td>
-              <td>{{ vcard.max_debit }}</td>
-            </tr>
-          </tbody>
-        </table>
-        <hr>
-      </div>
-      <div v-else>
-        <p class="text-center">No vCards found.</p>
-      </div>
+        <h1 class="text-center">vCard Details</h1>
+        <div v-if="vcards.length > 0">
+            <table class="table table-striped table-bordered">
+                <thead class="thead-dark">
+                    <tr>
+                        <th>Phone Number</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Balance</th>
+                        <th>Max Debit</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="vcard in vcards" :key="vcard.phone_number">
+                        <td>{{ vcard.phone_number }}</td>
+                        <td>{{ getFirstNameLastName(vcard.name) }}</td>
+                        <td>{{ vcard.email }}</td>
+                        <td>{{ vcard.balance }}</td>
+                        <td>{{ vcard.max_debit }}</td>
+                    </tr>
+                </tbody>
+            </table>
+            <hr />
+        </div>
+        <div v-else>
+            <p class="text-center">No vCards found.</p>
+        </div>
     </div>
-  </template>
+</template>
 <script>
 import axios from "axios"
 
@@ -52,6 +52,14 @@ export default {
                 .catch((error) => {
                     console.error("Error fetching vCards:", error)
                 })
+        },
+        getFirstNameLastName(fullName) {
+            const names = fullName.split(" ")
+            if (names.length >= 2) {
+                return names[0] + " " + names[names.length - 1]
+            } else {
+                return fullName
+            }
         },
     },
 }
