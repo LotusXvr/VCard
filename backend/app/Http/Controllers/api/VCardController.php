8a422f53591a528ca8c42cf98c79e1b363d2eccd
Lucas Remigio <2211034@my.ipleiria.pt>
2023-10-30 13:46:50 +0000

    ver os vcards na web

---
+++ backend/app/Http/Controllers/api/VCardController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\VCard;
+
+class VCardController extends Controller
+{
+    public function getVCards(VCard $vcard)
+    {
+        return $vcard;
+    }
+
+    public function index()
+    {
+        return VCard::all();
+    }
+}
---
+++ backend/app/Models/VCard.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+
+class VCard extends Model
+{
+    use HasFactory;
+    use SoftDeletes;
+
+    protected $table = 'vcards';
+    public $timestamps = true;
+    protected $primaryKey = 'phone_number';
+
+    protected $fillable = [
+        'phone_number',
+        'name',
+        'email',
+        'photo_url',
+        'password',
+        'confirmation_code',
+        'blocked',
+        'balance',
+        'max_debit',
+        'custom_options',
+        'custom_data',
+    ];
+
+    protected $dates = ['created_at', 'deleted_at', 'updated_at'];
+
+
+}
+
