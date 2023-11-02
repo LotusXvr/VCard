9533610ac1b22321988b14cd709154352f01a09a
Lucas <2211034@my.ipleiria.pt>
2023-11-02 21:58:04 +0000

    uniformização de MAC com WINDOWS

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -1,4 +1,16 @@
-export default {
-    baseAPI: "http://localhost/api",
-    userId: 1,
+let baseAPI;
+const userAgent = navigator.userAgent;
+
+if (/Win/i.test(userAgent)) {
+    baseAPI = "http://backend.test/api"; // Windows
+} else if (/Mac/i.test(userAgent)) {
+    baseAPI = "http://localhost/api"; // Mac
+} else {
+    // Default value for other platforms
+    baseAPI = "http://backend.test/api";
 }
+
+export default {
+    baseAPI,
+    userId: 1,
+};
