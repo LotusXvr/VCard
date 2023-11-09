00e78ee95e0e3fbd40b59c3c8ff388416b0ae00c
Lucas <2211034@my.ipleiria.pt>
2023-11-09 12:04:07 +0000

    implementação da template

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -76,8 +76,7 @@ const deleteVCard = async (vcard) => {
             fetchVCards()
             success.value = "VCard deleted successfully" // show success error
             error.value = null
-        }
-        catch (e) {
+        } catch (e) {
             success.value = null // clear success message
             error.value = e.response.data.errors // Capture and display API validation errors
         }
@@ -91,64 +90,299 @@ onMounted(() => {
 })
 </script>
 
-<template>
+<!-- <template>
     <div>
-        <div
-            style="
-                display: flex;
-                flex-direction: row;
-                align-items: center;
-                justify-content: center;
-                margin-top: 5%;
-            "
-        >
-            <button @click="showUsers" class="btn btn-primary">Show Users</button>
-            <button @click="showVCards" class="btn btn-primary">Show vCards</button>
-            <button @click="clearView" class="btn btn-primary">Clear View</button>
-        </div>
+        
 
         <createVCard @AddVCard="addVCard"></createVCard>
         <createUser @AddUser="addUser"></createUser>
 
-        <!-- Display error messages, if any -->
-        <div v-if="error">
-            <div class="alert alert-danger">
-                <ul>
-                    <li v-for="(message, field) in error" :key="field">
-                        {{ field }}: {{ message[0] }}
+        
+
+        
+
+        
+    </div>
+</template> -->
+
+<template>
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark sticky-top flex-md-nowrap p-0 shadow">
+        <div class="container-fluid">
+            <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="#">
+                <img
+                    src="@/assets/logo.svg"
+                    alt=""
+                    width="30"
+                    height="24"
+                    class="d-inline-block align-text-top"
+                />
+                VCard
+            </a>
+            <button
+                id="buttonSidebarExpandId"
+                class="navbar-toggler"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#sidebarMenu"
+                aria-controls="sidebarMenu"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+            >
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div v-if="error">
+                <div class="alert alert-danger">
+                    <ul>
+                        <li v-for="(message, field) in error" :key="field">
+                            {{ field }}: {{ message[0] }}
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
+            <div v-if="success">
+                <div class="alert alert-success">
+                    <ul>
+                        <li>{{ success }}</li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="collapse navbar-collapse justify-content-end">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#"
+                            ><i class="bi bi-person-check-fill"></i>
+                            Register
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#"
+                            ><i class="bi bi-box-arrow-in-right"></i>
+                            Login
+                        </a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a
+                            class="nav-link dropdown-toggle"
+                            href="#"
+                            id="navbarDropdownMenuLink"
+                            role="button"
+                            data-bs-toggle="dropdown"
+                            aria-expanded="false"
+                        >
+                            <img
+                                src="@/assets/avatar-exemplo-1.jpg"
+                                class="rounded-circle z-depth-0 avatar-img"
+                                alt="avatar image"
+                            />
+                            <span class="avatar-text">User Name</span>
+                        </a>
+                        <ul
+                            class="dropdown-menu dropdown-menu-dark dropdown-menu-end"
+                            aria-labelledby="navbarDropdownMenuLink"
+                        >
+                            <li>
+                                <a class="dropdown-item" href="#"
+                                    ><i class="bi bi-person-square"></i>Profile</a
+                                >
+                            </li>
+                            <li>
+                                <a class="dropdown-item" href="#"
+                                    ><i class="bi bi-key-fill"></i>Change password</a
+                                >
+                            </li>
+                            <li>
+                                <hr class="dropdown-divider" />
+                            </li>
+                            <li>
+                                <a class="dropdown-item" href="#"
+                                    ><i class="bi bi-arrow-right"></i>Logout</a
+                                >
+                            </li>
+                        </ul>
                     </li>
                 </ul>
             </div>
         </div>
+    </nav>
 
-        <!-- Display success messages, if any -->
-        <div v-if="success">
-            <div class="alert alert-success">
-                <ul>
-                    <li>{{ success }}</li>
-                </ul>
-            </div>
+    <div class="container-fluid">
+        <div class="row">
+            <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
+                <div class="position-sticky pt-3">
+                    <ul class="nav flex-column">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">
+                                <i class="bi bi-house"></i>
+                                Dashboard
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">
+                                <i class="bi bi-list-stars"></i>
+                                VCards
+                            </a>
+                        </li>
+                        <li class="nav-item d-flex justify-content-between align-items-center pe-3">
+                            <a @click="showUsers" class="nav-link w-100 me-3" href="#">
+                                <i class="bi bi-list-check"></i>
+                                Users
+                            </a>
+                            <a class="link-secondary" href="#" aria-label="Add a new task">
+                                <i class="bi bi-xs bi-plus-circle"></i>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">
+                                <i class="bi bi-files"></i>
+                                Transações
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">
+                                <i class="bi bi-people"></i>
+                                Enviar dinheiro
+                            </a>
+                        </li>
+                    </ul>
+
+                    <h6
+                        class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted"
+                    >
+                        <span>Histórico</span>
+                        <a class="link-secondary" href="#" aria-label="Add a new project">
+                            <i class="bi bi-xs bi-plus-circle"></i>
+                        </a>
+                    </h6>
+                    <ul class="nav flex-column mb-2">
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">
+                                <i class="bi bi-file-ruled"></i>
+                                Meu cartão
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#"
+                                ><i class="bi bi-file-ruled"></i>
+                                Minhas transações
+                            </a>
+                        </li>
+                    </ul>
+
+                    <div class="d-block d-md-none">
+                        <h6
+                            class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted"
+                        >
+                            <span>User</span>
+                        </h6>
+                        <ul class="nav flex-column mb-2">
+                            <li class="nav-item">
+                                <a class="nav-link" href="#"
+                                    ><i class="bi bi-person-check-fill"></i>
+                                    Register
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">
+                                    <i class="bi bi-box-arrow-in-right"></i>
+                                    Login
+                                </a>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <a
+                                    class="nav-link dropdown-toggle"
+                                    href="#"
+                                    id="navbarDropdownMenuLink2"
+                                    role="button"
+                                    data-bs-toggle="dropdown"
+                                    aria-expanded="false"
+                                >
+                                    <img
+                                        src="@/assets/avatar-exemplo-1.jpg"
+                                        class="rounded-circle z-depth-0 avatar-img"
+                                        alt="avatar image"
+                                    />
+                                    <span class="avatar-text">User Name</span>
+                                </a>
+                                <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink2">
+                                    <li>
+                                        <a class="dropdown-item" href="#">
+                                            <i class="bi bi-person-square"></i>Profile</a
+                                        >
+                                    </li>
+                                    <li>
+                                        <a class="dropdown-item" href="#">
+                                            <i class="bi bi-key-fill"></i>
+                                            Change password
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <hr class="dropdown-divider" />
+                                    </li>
+                                    <li>
+                                        <a class="dropdown-item" href="#">
+                                            <i class="bi bi-arrow-right"></i>Logout
+                                        </a>
+                                    </li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+
+            <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+                <dashboard>
+                    <div v-if="showingUsers">
+                        <h2>Users</h2>
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th>Name</th>
+                                    <th>Email</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr v-for="(user, index) in users" :key="index">
+                                    <td>{{ user.name }}</td>
+                                    <td>{{ user.email }}</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </dashboard>
+            </main>
         </div>
-
-        <!-- Display Users -->
-        <div v-if="showingUsers">
-            <h2>Users</h2>
-            <table class="table">
-                <thead>
-                    <tr>
-                        <th>Name</th>
-                        <th>Email</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr v-for="(user, index) in users" :key="index">
-                        <td>{{ user.name }}</td>
-                        <td>{{ user.email }}</td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
-
-        
     </div>
 </template>
+
+<style>
+@import "./assets/dashboard.css";
+
+.avatar-img {
+    margin: -1.2rem 0.8rem -2rem 0.8rem;
+    width: 3.3rem;
+    height: 3.3rem;
+}
+
+.avatar-text {
+    line-height: 2.2rem;
+    margin: 1rem 0.5rem -2rem 0;
+    padding-top: 1rem;
+}
+
+.dropdown-item {
+    font-size: 0.875rem;
+}
+
+.btn:focus {
+    outline: none;
+    box-shadow: none;
+}
+
+#sidebarMenu {
+    overflow-y: auto;
+}
+</style>
