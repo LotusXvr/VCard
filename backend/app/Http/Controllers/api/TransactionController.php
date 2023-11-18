3c1d108a8ef2adf9b7e4b9ad506316861963ec45
Lucas <2211034@my.ipleiria.pt>
2023-11-18 17:00:22 +0000

    Validações de transação
    - corrigir bug the transaction pair nao estar a ser preenchido,
    - atualizar o balance de ambas as contas implicitas na transação,
    - validar se o confirmation code é o presente do vcard
    - validar se utilizador a enviar dinheiro tem saldo suficiente

--- backend/app/Http/Controllers/api/TransactionController.php
+++ backend/app/Http/Controllers/api/TransactionController.php
@@ -10,120 +10,144 @@ use Illuminate\Support\Facades\DB;
 
 class TransactionController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        return Transaction::all();
+  /**
+   * Display a listing of the resource.
+   */
+  public function index()
+  {
+    return Transaction::all();
 
+  }
+
+  /**
+   * Display the specified resource.
+   */
+  public function show(Transaction $transaction)
+  {
+    return $transaction;
+  }
+
+  /**
+   * Store a newly created resource in storage.
+   */
+  public function store(Request $request)
+  {
+    /*
+     * - recebe:
+     * vcard, value, confirmation_code e payment_reference(vcard destino ou referencia)
+     * - precisa de preencher:
+     * vcard, date, datetime, type, value, old_balance, new_balamce, payment_type,
+     * payment_reference, pair_transaction, pair_vcard, category_id, description (nao obrigatorio)
+     *
+     * no caso da referencia destino ser um vcard, cria uma outra transação ligada por
+     * pair_transaction e pair_vcard (null em caso contrario)
+     * sabemos que o destinatário é vcard se o parametro payment_type for VCARD
+     */
+
+    // FIRST VALIDATIONS
+
+    // verify if confirmation code is the correct one
+    // having in mind the confirmation_code on the database is hashed
+    $vcardOrigin = VCard::where('phone_number', $request->vcard)->first();
+    if (!password_verify($request->confirmation_code, $vcardOrigin->confirmation_code)) {
+      return response()->json(['message' => 'Código de confirmação inválido'], 401);
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(Transaction $transaction)
-    {
-        return $transaction;
+    // verify if sender has enough money on account balance
+    if ($vcardOrigin->balance < $request->value) {
+      return response()->json(['message' => 'Saldo insuficiente'], 401);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        /*
-         * - recebe:
-         * vcard, value, confirmation_code e payment_reference(vcard destino ou referencia)
-         * - precisa de preencher:
-         * vcard, date, datetime, type, value, old_balance, new_balamce, payment_type,
-         * payment_reference, pair_transaction, pair_vcard, category_id, description (nao obrigatorio)
-         *
-         * no caso da referencia destino ser um vcard, cria uma outra transação ligada por
-         * pair_transaction e pair_vcard (null em caso contrario)
-         */
+    // VCARD
 
+    if ($request->payment_type == 'VCARD') {
+      // Verify if destination vcard exists
+      $destinVCardExists = VCard::where('phone_number', $request->payment_reference)
+        ->whereNull('deleted_at')
+        ->first();
+      if (!$destinVCardExists) {
+        return response()->json(['message' => 'VCard não existe'], 404);
+      }
 
+      try {
+        DB::transaction(function () use ($request) {
+          // Money sending transaction
+          $transaction1 = new Transaction();
+          $transaction1->vcard = $request->vcard;
+          $transaction1->date = date('Y-m-d');
+          $transaction1->datetime = date('Y-m-d H:i:s');
+          $transaction1->type = 'D'; // como o utilizador está a enviar dinheiro, a primeira operação é sempre Debito
+          $transaction1->value = $request->value;
+          $vcardBalance = VCard::where('phone_number', $request->vcard)->first()->balance;
+          $transaction1->old_balance = $vcardBalance;
+          $transaction1->new_balance = $vcardBalance - $request->value;
+          $transaction1->payment_type = 'VCARD';
+          $transaction1->payment_reference = $request->payment_reference;
+          $transaction1->pair_vcard = $request->payment_reference;
+          $transaction1->category_id = null;
+          $transaction1->description = null;
 
-        try {
-            DB::transaction(function () use ($request) {
-                $isDestinVCard = VCard::where('phone_number', $request->vcardDestination)
-                    ->whereNull('deleted_at')
-                    ->first();
-                if ($isDestinVCard) {
+          // Money reception transaction
+          $transaction2 = new Transaction();
+          $transaction2->vcard = $request->payment_reference;
+          $transaction2->date = date('Y-m-d');
+          $transaction2->datetime = date('Y-m-d H:i:s');
+          $transaction2->type = 'C'; // tendo em conta esta operaçao ser a inversa, esta será de Crédito
+          $transaction2->value = $request->value;
+          $payment_referenceBalance = VCard::where('phone_number', $request->payment_reference)->first()->balance;
+          $transaction2->old_balance = $payment_referenceBalance;
+          $transaction2->new_balance = $payment_referenceBalance + $request->value;
+          $transaction2->payment_type = 'VCARD';
+          $transaction2->payment_reference = $request->vcard;
+          $transaction2->pair_transaction = $transaction1->id;
+          $transaction2->pair_vcard = $request->vcard;
+          $transaction2->category_id = null;
+          $transaction2->description = null;
 
-                    // Transação de envio
-                    $transaction1 = new Transaction();
-                    $transaction1->vcard = $request->vcardOrigin;
-                    $transaction1->date = date('Y-m-d');
-                    $transaction1->datetime = date('Y-m-d H:i:s');
-                    $transaction1->type = 'D'; // como o utilizador está a enviar dinheiro, a primeira operação é sempre Debito
-                    $transaction1->value = $request->value;
-                    $vcardOriginBalance = VCard::where('phone_number', $request->vcardOrigin)->first()->balance;
-                    $transaction1->old_balance = $vcardOriginBalance;
-                    $transaction1->new_balance = $vcardOriginBalance - $request->value;
-                    $transaction1->payment_type = 'VCARD';
-                    $transaction1->payment_reference = $request->vcardDestination;
-                    $transaction1->pair_vcard = $request->vcardDestination;
-                    $transaction1->category_id = null;
-                    $transaction1->description = null;
+          // Save transactions to get their id's
+          $transaction1->save();
+          $transaction2->save();
 
-                    // Transação de receção
-                    $transaction2 = new Transaction();
-                    $transaction2->vcard = $request->vcardDestination;
-                    $transaction2->date = date('Y-m-d');
-                    $transaction2->datetime = date('Y-m-d H:i:s');
-                    $transaction2->type = 'C'; // tendo em conta esta operaçao ser a inversa, esta será de Crédito
-                    $transaction2->value = $request->value;
-                    $vcardDestinationBalance = VCard::where('phone_number', $request->vcardDestination)->first()->balance;
-                    $transaction2->old_balance = $vcardDestinationBalance;
-                    $transaction2->new_balance = $vcardDestinationBalance + $request->value;
-                    $transaction2->payment_type = 'VCARD';
-                    $transaction2->payment_reference = $request->vcardOrigin;
-                    $transaction2->pair_transaction = $transaction1->id;
-                    $transaction2->pair_vcard = $request->vcardOrigin;
-                    $transaction2->category_id = null;
-                    $transaction2->description = null;
+          // Update pair_transaction properties
+          $transaction1->pair_transaction = $transaction2->id;
+          $transaction2->pair_transaction = $transaction1->id;
 
-                    $transaction1->pair_transaction = $transaction2->id;
-                    $transaction2->pair_transaction = $transaction1->id;
+          // Save transactions again to update pair_transaction values
+          $transaction1->save();
+          $transaction2->save();
 
+          // Update both individual's balances
+          VCard::where('phone_number', $request->vcard)->update(['balance' => $transaction1->new_balance]);
+          VCard::where('phone_number', $request->payment_reference)->update(['balance' => $transaction2->new_balance]);
 
-                    $transaction1->save();
-                    $transaction2->save();
+        });
 
-                }
+        return response()->json(['message' => 'Transação efetuada com sucesso'], 200);
 
-            });
-
-
-
-            return response()->json(['message' => 'Transação efetuada com sucesso'], 200);
-
-        } catch (\Exception $e) {
-            return response()->json([
-                'message' => 'Erro ao criar transação',
-                'error' => $e->getMessage()
-            ], 500);
-        }
+      } catch (\Exception $e) {
+        return response()->json([
+          'message' => 'Erro ao criar transação',
+          'error' => $e->getMessage()
+        ], 500);
+      }
     }
+  }
 
 
 
+  /**
+   * Update the specified resource in storage.
+   */
+  public function update(Request $request, Transaction $transaction)
+  {
+    //
+  }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, Transaction $transaction)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Transaction $transaction)
-    {
-        //
-    }
+  /**
+   * Remove the specified resource from storage.
+   */
+  public function destroy(Transaction $transaction)
+  {
+    //
+  }
 }
