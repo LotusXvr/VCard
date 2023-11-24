14e760eb6bfedcb36f01d5fc99f3df27020f3dd2
Diogo Guerra <2211027@my.ipleiria.pt>
2023-11-24 20:34:03 +0000

    Update main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -18,23 +18,13 @@ const app = createApp(App)
 const apiDomain = import.meta.env.VITE_API_DOMAIN
 const wsConnection = import.meta.env.VITE_WS_CONNECTION
 
-let serverBaseUrl
 const userAgent = navigator.userAgent
-
-if (/Win/i.test(userAgent)) {
-  serverBaseUrl = 'http://backend.test' // Windows
-} else if (/Mac/i.test(userAgent)) {
-  serverBaseUrl = 'http://localhost' // Mac
-} else {
-  // Default value for other platforms
-  serverBaseUrl = 'http://backend.test'
-}
 
 // app.provide('serverBaseUrl', serverBaseUrl)
 // app.provide('socket', io('http://localhost:8080'))
 app.provide('serverUrl', `${apiDomain}/api`)
 app.provide('socket', io(wsConnection))
-axios.defaults.baseURL = serverBaseUrl + '/api'
+axios.defaults.baseURL = apiDomain + '/api'
 axios.defaults.headers.common['Content-type'] = 'application/json'
 
 app.use(createPinia())
