ed83e1ddaa7a3403bc3ff3f07c0379a18bf11807
Diogo Guerra <2211027@my.ipleiria.pt>
2023-12-12 15:33:46 +0000

    Correção de bug ao registar

--- frontend/src/components/vcard/VCard.vue
+++ frontend/src/components/vcard/VCard.vue
@@ -1,132 +1,130 @@
 <script setup>
-import axios from "axios"
-import { useToast } from "vue-toastification"
-import { useUserStore } from "../../stores/user"
-import { ref, watch, onMounted } from "vue"
-import VCardDetail from "./VCardDetail.vue"
-import { useRouter } from "vue-router"
+import axios from 'axios'
+import { useToast } from 'vue-toastification'
+import { useUserStore } from '../../stores/user'
+import { ref, watch, onMounted } from 'vue'
+import VCardDetail from './VCardDetail.vue'
+import { useRouter } from 'vue-router'
 
 const toast = useToast()
 const router = useRouter()
 const userStore = useUserStore()
 
 const props = defineProps({
-    phone_number: {
-        type: Number,
-        default: null,
-    },
+  phone_number: {
+    type: Number,
+    default: null
+  }
 })
 
 const newVCard = () => {
-    return {
-        phone_number: null,
-        name: "",
-        email: "",
-        photo_url: null,
-        password: "",
-        password_confirmation: "",
-        confirmation_code: "",
-    }
+  return {
+    phone_number: null,
+    name: '',
+    email: '',
+    photo_url: null,
+    password: '',
+    password_confirmation: '',
+    confirmation_code: ''
+  }
 }
 
 // const confirmationLeaveDialog = ref({})
 const vcard = ref(newVCard())
 const errors = ref(null)
 
-let originalValueStr = ""
+let originalValueStr = ''
 
 const inserting = (phone_number) => !phone_number || phone_number < 0
 
 const loadVCard = async (phone_number) => {
-    originalValueStr = ""
-    errors.value = null
-    if (inserting(phone_number)) {
-        vcard.value = newVCard()
-    } else {
-        try {
-            const response = await axios.get("vcards/" + phone_number)
-            vcard.value = response.data.data
-            console.log(response.data.data)
-            originalValueStr = JSON.stringify(vcard.value)
-        } catch (error) {
-            console.log(error)
-        }
+  originalValueStr = ''
+  errors.value = null
+  if (inserting(phone_number)) {
+    vcard.value = newVCard()
+  } else {
+    try {
+      const response = await axios.get('vcards/' + phone_number)
+      vcard.value = response.data.data
+      console.log(response.data.data)
+      originalValueStr = JSON.stringify(vcard.value)
+    } catch (error) {
+      console.log(error)
     }
+  }
 }
 
 const save = async (vcardToSave) => {
-    errors.value = null
-    if (inserting(props.phone_number)) {
-        try {
-            const response = await axios.post("vcard", vcardToSave)
-            vcard.value = response.data.data
-            originalValueStr = JSON.stringify(vcard.value)
-            toast.success("VCard #" + vcard.value.phone_number + " was registered successfully.")
-            if (userStore.login != true) {
-                await userStore.login({
-                    username: vcard.value.phone_number.toString(),
-                    password: vcardToSave.password,
-                })
-                router.push({ name: "Home" })
-            }
-
-            router.back()
-        } catch (error) {
-            console.log(error.response.data.errors)
-            if (error.response.status == 422) {
-                toast.error(error.response.data.message)
-            } else {
-                toast.error(error.response.data.message)
-            }
-        }
-    } else {
-        try {
-            const response = await axios.put("vcards/" + props.phone_number, vcardToSave)
-            vcard.value = response.data.data
-            originalValueStr = JSON.stringify(vcard.value)
-            toast.success("VCard #" + vcard.value.phone_number + " was updated successfully.")
-            if (vcard.value.phone_number == userStore.userId) {
-                await userStore.loadUser()
-            }
-            router.back()
-        } catch (error) {
-            if (error.response.status == 422) {
-                errors.value = error.response.data.errors
-                // toast.error('VCard #' + props.phone_number + ' was not updated due to validation errors!')
-                // console.log(error.response.data.errors['phone_number'][0])
-
-                // show first error message only ( not only phone_number but all fields)
-                for (const [key, value] of Object.entries(error.response.data.errors)) {
-                    toast.error(value[0])
-                    break
-                }
-            } else {
-                toast.error(
-                    "VCard #" +
-                        props.phone_number +
-                        " was not updated due to unknown server error!",
-                )
-            }
-        }
+  errors.value = null
+  if (inserting(props.phone_number)) {
+    try {
+      const response = await axios.post('vcard', vcardToSave)
+      vcard.value = response.data.data
+      originalValueStr = JSON.stringify(vcard.value)
+      toast.success('VCard #' + vcard.value.phone_number + ' was registered successfully.')
+      if (userStore.login != true) {
+        await userStore.login({
+          username: vcard.value.phone_number.toString(),
+          password: vcardToSave.password
+        })
+        router.push({ name: 'Home' })
+      } else {
+        router.back()
+      }
+    } catch (error) {
+      console.log(error.response.data.errors)
+      if (error.response.status == 422) {
+        toast.error(error.response.data.message)
+      } else {
+        toast.error(error.response.data.message)
+      }
     }
+  } else {
+    try {
+      const response = await axios.put('vcards/' + props.phone_number, vcardToSave)
+      vcard.value = response.data.data
+      originalValueStr = JSON.stringify(vcard.value)
+      toast.success('VCard #' + vcard.value.phone_number + ' was updated successfully.')
+      if (vcard.value.phone_number == userStore.userId) {
+        await userStore.loadUser()
+      }
+      router.back()
+    } catch (error) {
+      if (error.response.status == 422) {
+        errors.value = error.response.data.errors
+        // toast.error('VCard #' + props.phone_number + ' was not updated due to validation errors!')
+        // console.log(error.response.data.errors['phone_number'][0])
+
+        // show first error message only ( not only phone_number but all fields)
+        for (const [key, value] of Object.entries(error.response.data.errors)) {
+          toast.error(value[0])
+          break
+        }
+      } else {
+        toast.error(
+          'VCard #' + props.phone_number + ' was not updated due to unknown server error!'
+        )
+      }
+    }
+  }
 }
 
 const cancel = () => {
-    originalValueStr = JSON.stringify(vcard.value)
-    router.back()
+  originalValueStr = JSON.stringify(vcard.value)
+  router.back()
 }
 
 watch(
-    () => props.phone_number,
-    (newValue) => {
-        loadVCard(newValue)
-    },
-    { immediate: true },
+  () => props.phone_number,
+  (newValue) => {
+    loadVCard(newValue)
+  },
+  { immediate: true }
 )
 
 onMounted(() => {
-    loadVCard(userStore.userPhoneNumber)
-    console.log("123" + userStore.userPhoneNumber)
+  loadVCard(userStore.userPhoneNumber)
+  console.log('123' + userStore.userPhoneNumber)
 })
 
 // let nextCallBack = null
@@ -151,15 +149,15 @@ onMounted(() => {
 </script>
 
 <template>
-    <!-- <confirmation-dialog ref="confirmationLeaveDialog" confirmationBtn="Discard changes and leave"
+  <!-- <confirmation-dialog ref="confirmationLeaveDialog" confirmationBtn="Discard changes and leave"
         msg="Do you really want to leave? You have unsaved changes!" @confirmed="leaveConfirmed">
     </confirmation-dialog> -->
-    <VCardDetail
-        :vcard="vcard"
-        :errors="errors"
-        :inserting="inserting(phone_number)"
-        @save="save"
-        @cancel="cancel"
-    >
-    </VCardDetail>
+  <VCardDetail
+    :vcard="vcard"
+    :errors="errors"
+    :inserting="inserting(phone_number)"
+    @save="save"
+    @cancel="cancel"
+  >
+  </VCardDetail>
 </template>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -20,199 +20,199 @@ import DismissVCard from '../components/vcard/DismissVCard.vue'
 let handlingFirstRoute = true
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
-    routes: [
-        {
-            path: "/",
-            name: "Home",
-            component: HomeView,
-        },
-        {
-            path: "/login",
-            name: "Login",
-            component: Login,
-        },
-        {
-            path: "/password",
-            name: "ChangePassword",
-            component: ChangePassword,
-        },
-        {
-            path: "/dashboard",
-            name: "Dashboard",
-            component: Dashboard,
-            props: (route) => ({ phone_number: parseInt(route.params.phone_number)}),
-        },
-        {
-            path: "/vcards",
-            name: "VCards",
-            component: VCards,
-            props: { onlyCurrentVCards: false, vcardsTitle: "VCards" },
-        },
-        {
-            path: "/vcard/new",
-            name: "NewVCard",
-            component: VCard,
-            props: { id: -1 },
-        },
-        {
-            path: "/vcard/:phone_number",
-            name: "VCard",
-            component: VCard,
-            props: (route) => ({ phone_number: parseInt(route.params.phone_number), details: true }),
-        },
-        {
-            path: "/profile/:phone_number",
-            name: "ProfileVCard",
-            component: VCard,
-            props: (route) => ({ phone_number: parseInt(route.params.phone_number)}),
-        },
-        {
-            path: "/vcard/dismiss_vcard",
-            name: "DismissVCard",
-            component: DismissVCard,
-        },
-        {
-            path: "/vcard/:phone_number/confirmation_code",
-            name: "ConfirmationCode",
-            component: ConfirmationCode,
-            props: (route) => ({ phone_number: parseInt(route.params.phone_number) }),
-        },
-        {
-            path: "/vcards/admin/:phone_number",
-            name: "VCardUpdate",
-            component: VCard,
-            props: (route) => ({ phone_number: parseInt(route.params.phone_number) }),
-        },
-        {
-            path: "/transaction/new",
-            name: "NewTransaction",
-            component: Transaction,
-            props: { id: -1 },
-        },
-        {
-            path: "/transaction/credit",
-            name: "NewCreditTransaction",
-            component: Transaction,
-            props: { id: -2 },
-        },
-        {
-            path: "/transaction/:id",
-            name: "Transaction",
-            component: Transaction,
-            props: (route) => ({ id: parseInt(route.params.id) }),
-        },
-        {
-            path: "/vcard/transactions",
-            name: "Transactions",
-            component: Transactions,
-        },
-        {
-            path: "/vcard/categories",
-            name: "Categories",
-            component: Categories,
-        },
-        {
-            path: "/categories/new",
-            name: "NewCategory",
-            component: Category,
-            props: { id: -1 },
-        },
-        {
-            path: "/category/:id",
-            name: "Category",
-            component: Category,
-            props: (route) => ({ id: parseInt(route.params.id) }),
-        },
-        {
-            path: "/default-categories",
-            name: "DefaultCategories",
-            component: DefaultCategories,
-        },
-        {
-            path: "/default-categories/new",
-            name: "NewDefaultCategory",
-            component: DefaultCategory,
-            props: { id: -1 },
-        },
-        {
-            path: "/default-category/:id",
-            name: "DefaultCategory",
-            component: DefaultCategory,
-            props: (route) => ({ id: parseInt(route.params.id) }),
-        },
-        {
-            path: "/admins", // Altere de '/users' para '/administrators'
-            name: "Users",
-            component: Users,
-        },
-        {
-            path: "/admins/:id", // Altere de '/users/:id' para '/administrators/:id'
-            name: "User",
-            component: User,
-            props: (route) => ({ id: parseInt(route.params.id) }),
-        },
-        {
-            path: "/admins/new", // Altere de '/users/new' para '/administrators/new'
-            name: "NewUser",
-            component: User,
-            props: { id: -1 },
-        },
-    ],
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: HomeView
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login
+    },
+    {
+      path: '/password',
+      name: 'ChangePassword',
+      component: ChangePassword
+    },
+    {
+      path: '/dashboard',
+      name: 'Dashboard',
+      component: Dashboard,
+      props: (route) => ({ phone_number: parseInt(route.params.phone_number) })
+    },
+    {
+      path: '/vcards',
+      name: 'VCards',
+      component: VCards,
+      props: { onlyCurrentVCards: false, vcardsTitle: 'VCards' }
+    },
+    {
+      path: '/vcard/new',
+      name: 'NewVCard',
+      component: VCard,
+      props: { id: -1 }
+    },
+    {
+      path: '/vcard/:phone_number',
+      name: 'VCard',
+      component: VCard,
+      props: (route) => ({ phone_number: parseInt(route.params.phone_number), details: true })
+    },
+    {
+      path: '/profile/:phone_number',
+      name: 'ProfileVCard',
+      component: VCard,
+      props: (route) => ({ phone_number: parseInt(route.params.phone_number) })
+    },
+    {
+      path: '/vcard/dismiss_vcard',
+      name: 'DismissVCard',
+      component: DismissVCard
+    },
+    {
+      path: '/vcard/:phone_number/confirmation_code',
+      name: 'ConfirmationCode',
+      component: ConfirmationCode,
+      props: (route) => ({ phone_number: parseInt(route.params.phone_number) })
+    },
+    {
+      path: '/vcards/admin/:phone_number',
+      name: 'VCardUpdate',
+      component: VCard,
+      props: (route) => ({ phone_number: parseInt(route.params.phone_number) })
+    },
+    {
+      path: '/transaction/new',
+      name: 'NewTransaction',
+      component: Transaction,
+      props: { id: -1 }
+    },
+    {
+      path: '/transaction/credit',
+      name: 'NewCreditTransaction',
+      component: Transaction,
+      props: { id: -2 }
+    },
+    {
+      path: '/transaction/:id',
+      name: 'Transaction',
+      component: Transaction,
+      props: (route) => ({ id: parseInt(route.params.id) })
+    },
+    {
+      path: '/vcard/transactions',
+      name: 'Transactions',
+      component: Transactions
+    },
+    {
+      path: '/vcard/categories',
+      name: 'Categories',
+      component: Categories
+    },
+    {
+      path: '/categories/new',
+      name: 'NewCategory',
+      component: Category,
+      props: { id: -1 }
+    },
+    {
+      path: '/category/:id',
+      name: 'Category',
+      component: Category,
+      props: (route) => ({ id: parseInt(route.params.id) })
+    },
+    {
+      path: '/default-categories',
+      name: 'DefaultCategories',
+      component: DefaultCategories
+    },
+    {
+      path: '/default-categories/new',
+      name: 'NewDefaultCategory',
+      component: DefaultCategory,
+      props: { id: -1 }
+    },
+    {
+      path: '/default-category/:id',
+      name: 'DefaultCategory',
+      component: DefaultCategory,
+      props: (route) => ({ id: parseInt(route.params.id) })
+    },
+    {
+      path: '/admins', // Altere de '/users' para '/administrators'
+      name: 'Users',
+      component: Users
+    },
+    {
+      path: '/admins/:id', // Altere de '/users/:id' para '/administrators/:id'
+      name: 'User',
+      component: User,
+      props: (route) => ({ id: parseInt(route.params.id) })
+    },
+    {
+      path: '/admins/new', // Altere de '/users/new' para '/administrators/new'
+      name: 'NewUser',
+      component: User,
+      props: { id: -1 }
+    }
+  ]
 })
 
 router.beforeEach(async (to, from, next) => {
-    const userStore = useUserStore()
+  const userStore = useUserStore()
 
-    if (handlingFirstRoute) {
-        handlingFirstRoute = false
-        await userStore.restoreToken()
-    }
+  if (handlingFirstRoute) {
+    handlingFirstRoute = false
+    await userStore.restoreToken()
+  }
 
-    // Rotas públicas que podem ser acessadas por usuários não autenticados
-    const publicRoutes = ['Login', 'Home', 'NewVCard']
+  // Rotas públicas que podem ser acessadas por usuários não autenticados
+  const publicRoutes = ['Login', 'Home', 'NewVCard']
 
-    if (publicRoutes.includes(to.name)) {
-        next()
-        return
-    }
-
-    // Redirecionar para a página de login se o usuário não estiver autenticado
-    if (!userStore.user) {
-        next({ name: 'Login' })
-        return
-    }
-
-    if (
-        to.name === 'Transactions' &&
-        (userStore.userType !== 'V' || userStore.userId === parseInt(to.params.id))
-    ) {
-        next()
-        return
-    }
-
-    if (to.name === 'ConfirmationCode' && userStore.userType !== 'V') {
-        next({ name: 'Home' })
-    }
-
-    if (to.name === 'DismissVCard' && userStore.userType !== 'V') {
-        next({ name: 'Home' })
-    }
-
-    if (to.name === 'Dashboard' && userStore.userType !== 'A') {
-        next({ name: 'Home' })
-        return
-    }
-    
-    if (
-        to.name === 'User' &&
-        (userStore.userType === 'A' || userStore.userId === parseInt(to.params.id))
-    ) {
-        next()
-        return
-    }
-
+  if (publicRoutes.includes(to.name)) {
     next()
+    return
+  }
+
+  // Redirecionar para a página de login se o usuário não estiver autenticado
+  if (!userStore.user) {
+    next({ name: 'Login' })
+    return
+  }
+
+  if (
+    to.name === 'Transactions' &&
+    (userStore.userType !== 'V' || userStore.userId === parseInt(to.params.id))
+  ) {
+    next()
+    return
+  }
+
+  if (to.name === 'ConfirmationCode' && userStore.userType !== 'V') {
+    next({ name: 'Home' })
+  }
+
+  if (to.name === 'DismissVCard' && userStore.userType !== 'V') {
+    next({ name: 'Home' })
+  }
+
+  if (to.name === 'Dashboard' && userStore.userType !== 'A') {
+    next({ name: 'Home' })
+    return
+  }
+
+  if (
+    to.name === 'User' &&
+    (userStore.userType === 'A' || userStore.userId === parseInt(to.params.id))
+  ) {
+    next()
+    return
+  }
+
+  next()
 })
 
 export default router
