e9b520ed063c126f795acbd3637e00226bf78b12
Diogo Guerra <2211027@my.ipleiria.pt>
2023-12-12 16:04:56 +0000

    Update Users.vue
    Mensagem de sucesso e erro ao eliminar vcard

--- frontend/src/components/user/Users.vue
+++ frontend/src/components/user/Users.vue
@@ -1,74 +1,71 @@
 <script setup>
-import axios from "axios"
-import { ref, onMounted } from "vue"
-import UserTable from "./UserTable.vue"
-import { useRouter } from "vue-router"
+import axios from 'axios'
+import { ref, onMounted } from 'vue'
+import UserTable from './UserTable.vue'
+import { useRouter } from 'vue-router'
+import { useToast } from 'vue-toastification'
 
 const router = useRouter()
-
+const toast = useToast()
 
 const users = ref([])
 
 const loadUsers = () => {
-    // Altere mais tarde quando a autenticação for implementada
-    axios
-        .get("admins")
-        .then((response) => {
-            users.value = response.data
-        })
-        .catch((error) => {
-            console.log(error)
-        })
+  // Altere mais tarde quando a autenticação for implementada
+  axios
+    .get('admins')
+    .then((response) => {
+      users.value = response.data
+    })
+    .catch((error) => {
+      toast.error(error.response.data.message)
+    })
 }
 
 const addUser = () => {
-    router.push({ name: "NewUser" }) // Certifique-se de ter uma rota chamada 'newUser'
+  router.push({ name: 'NewUser' }) // Certifique-se de ter uma rota chamada 'newUser'
 }
 
 const editUser = (user) => {
-    router.push({ name: "User", params: { id: user.id } }) // Certifique-se de ter uma rota chamada 'User'
+  router.push({ name: 'User', params: { id: user.id } }) // Certifique-se de ter uma rota chamada 'User'
 }
 
 const deleteUser = (user) => {
-    axios
-        .delete("admins/" + user.id)
-        .then(() => {
-            loadUsers()
-        })
-        .catch((error) => {
-            console.log(error)
-        })
+  axios
+    .delete('admins/' + user.id)
+    .then(() => {
+      toast.success('User deleted successfully')
+      loadUsers()
+    })
+    .catch((error) => {
+      toast.error(error.response.data.message)
+    })
 }
 
 onMounted(() => {
-    loadUsers()
+  loadUsers()
 })
 </script>
 
 <template>
-    <div class="d-flex justify-content-between">
-        <div class="mx-2">
-            <h3 class="mt-4">{{ usersTitle }}</h3>
-        </div>
+  <div class="d-flex justify-content-between">
+    <div class="mx-2">
+      <h3 class="mt-4">{{ usersTitle }}</h3>
     </div>
-    <hr />
-    <div class="mb-3 d-flex justify-content-between flex-wrap">
-        <div class="mx-2 mt-2 flex-grow-1 filter-div"></div>
-        <div class="mx-2 mt-2">
-            <router-link class="nav-link w-100 me-3" :to="{ name: 'NewUser' }">
-                <button type="button" class="btn btn-success px-4 btn-addtask" @click="addUser">
-                    <i class="bi bi-xs bi-plus-circle"></i>&nbsp; Add User
-                </button>
-            </router-link>
-        </div>
+  </div>
+  <hr />
+  <div class="mb-3 d-flex justify-content-between flex-wrap">
+    <div class="mx-2 mt-2 flex-grow-1 filter-div"></div>
+    <div class="mx-2 mt-2">
+      <router-link class="nav-link w-100 me-3" :to="{ name: 'NewUser' }">
+        <button type="button" class="btn btn-success px-4 btn-addtask" @click="addUser">
+          <i class="bi bi-xs bi-plus-circle"></i>&nbsp; Add User
+        </button>
+      </router-link>
     </div>
-    <div v-if="users.length > 0">
-        <UserTable
-            :users="users"
-            :showUserId="true"
-            @edit="editUser"
-            @delete="deleteUser"
-        ></UserTable>
-    </div>
-    <div v-else>Loading Users...</div>
+  </div>
+  <div v-if="users.length > 0">
+    <UserTable :users="users" :showUserId="true" @edit="editUser" @delete="deleteUser"></UserTable>
+  </div>
+  <div v-else>Loading Users...</div>
 </template>
