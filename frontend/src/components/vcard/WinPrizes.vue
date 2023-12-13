e2103f92dbe004d266f6ebc0b3eb8e3ca54cc2c1
Lucas <2211034@my.ipleiria.pt>
2023-12-13 12:59:00 +0000

    mais coisas

--- frontend/src/components/vcard/WinPrizes.vue
+++ frontend/src/components/vcard/WinPrizes.vue
@@ -30,7 +30,18 @@ const generatePrize = () => {
         prizeWon.value = "Nothing"
         badPrize.value = true
     } else {
-        const prizes = ["Prize 1", "Prize 2", "Prize 3", "Prize 4", "Prize 5", "Prize 6"]
+        const prizes = [
+            "0.01€",
+            "0.02€",
+            "0.10€",
+            "1€",
+            "Vale no Brasa Rio",
+            "Manicure gratuita",
+            "Viagem a Espanha",
+            "MacBook",
+            "Rolex",
+            "Tesla Model 3",
+        ]
         const randomIndex = Math.floor(Math.random() * prizes.length)
         prizeWon.value = prizes[randomIndex]
         goodPrize.value = true
@@ -58,7 +69,26 @@ const generatePrize = () => {
         <h4 v-if="badPrize">Sorry, you didn't win anything this time.</h4>
     </div>
 
-    <div class="text-center mt-3" v-if="goodPrize">
-        <h2>Ganhaste {{ prizeWon }}!</h2>
+    <div class="text-center mt-3 mb-3" v-if="goodPrize">
+        <!-- Display for good prizes with text on top of the smaller image -->
+        <div style="position: relative">
+            <img
+                src="/congratulations.jpg"
+                alt="Congratulations!"
+                style="width: 600px; height: auto"
+            />
+            <h2
+                style="
+                    position: absolute;
+                    top: 70%;
+                    left: 50%;
+                    transform: translate(-50%, -50%);
+                    color: white;
+                    text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.7);
+                "
+            >
+                You won {{ prizeWon }}!
+            </h2>
+        </div>
     </div>
 </template>
