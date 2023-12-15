9221bd3701e89cdc90f4bc4ea92627c6344de9aa
Diogo Guerra <2211027@my.ipleiria.pt>
2023-12-15 11:21:35 +0000

    No HomeView mostrar o maxDebit do Vcard

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,46 +1,46 @@
 <script setup>
-import axios from "axios"
-import { useToast } from "vue-toastification"
-import { useUserStore } from "../stores/user"
-import { ref, onMounted, computed, inject } from "vue"
+import axios from 'axios'
+import { useToast } from 'vue-toastification'
+import { useUserStore } from '../stores/user'
+import { ref, onMounted, computed, inject } from 'vue'
 
-import Chart from "chart.js/auto"
+import Chart from 'chart.js/auto'
 
 const toast = useToast()
 const userStore = useUserStore()
 const socket = inject('socket')
 
 const newVCard = () => {
-    return {
-        phone_number: null,
-        name: "",
-        email: "",
-        photo_url: null,
-        password: "",
-        password_confirmation: "",
-        confirmation_code: "",
-    }
+  return {
+    phone_number: null,
+    name: '',
+    email: '',
+    photo_url: null,
+    password: '',
+    password_confirmation: '',
+    confirmation_code: ''
+  }
 }
 socket.on('moneySentNotification', () => {
-    loadVCard(userStore.userPhoneNumber)
+  loadVCard(userStore.userPhoneNumber)
 })
 
 const vcard = ref(newVCard())
 const errors = ref(null)
 const transferAmount = ref(0)
 
-let originalValueStr = ""
+let originalValueStr = ''
 const loadVCard = async (phone_number) => {
-    originalValueStr
-    errors.value = null
-    try {
-        const response = await axios.get("vcards/" + phone_number)
-        vcard.value = response.data.data
-        console.log(response.data.data)
-        originalValueStr = JSON.stringify(vcard.value)
-    } catch (error) {
-        console.log(error)
-    }
+  originalValueStr
+  errors.value = null
+  try {
+    const response = await axios.get('vcards/' + phone_number)
+    vcard.value = response.data.data
+    console.log(response.data.data)
+    originalValueStr = JSON.stringify(vcard.value)
+  } catch (error) {
+    console.log(error)
+  }
 }
 
 // watch(
@@ -52,85 +52,85 @@ const loadVCard = async (phone_number) => {
 // )
 
 const reforcarPoupanca = async () => {
-    if (transferAmount.value <= 0) {
-        toast.error("Transfer amount must be greater than 0.")
-        return
-    }
+  if (transferAmount.value <= 0) {
+    toast.error('Transfer amount must be greater than 0.')
+    return
+  }
 
-    transferAmount.value = parseFloat(transferAmount.value).toFixed(2)
+  transferAmount.value = parseFloat(transferAmount.value).toFixed(2)
 
-    try {
-        await axios.post("vcards/" + userStore.userPhoneNumber + "/reforcarPoupanca", {
-            vcard: userStore.userPhoneNumber,
-            valor: transferAmount.value,
-        })
+  try {
+    await axios.post('vcards/' + userStore.userPhoneNumber + '/reforcarPoupanca', {
+      vcard: userStore.userPhoneNumber,
+      valor: transferAmount.value
+    })
 
-        toast.success("Savings reinforced sucessfully!")
-        vcard.value.balance = parseFloat(vcard.value.balance - transferAmount.value).toFixed(2)
-        const savings = parseFloat(vcard.value.savings)
-        vcard.value.savings = parseFloat(savings + transferAmount.value).toFixed(2)
-    } catch (error) {
-        toast.error(error.response.data.message || "An error occurred.")
-    }
+    toast.success('Savings reinforced sucessfully!')
+    vcard.value.balance = parseFloat(vcard.value.balance - transferAmount.value).toFixed(2)
+    const savings = parseFloat(vcard.value.savings)
+    vcard.value.savings = parseFloat(savings + transferAmount.value).toFixed(2)
+  } catch (error) {
+    toast.error(error.response.data.message || 'An error occurred.')
+  }
 }
 
 const retirarPoupanca = async () => {
-    if (transferAmount.value <= 0) {
-        toast.error("Transfer amount must be greater than 0.")
-        return
-    }
+  if (transferAmount.value <= 0) {
+    toast.error('Transfer amount must be greater than 0.')
+    return
+  }
 
-    transferAmount.value = parseFloat(transferAmount.value).toFixed(2)
+  transferAmount.value = parseFloat(transferAmount.value).toFixed(2)
 
-    try {
-        await axios.post("vcards/" + userStore.userPhoneNumber + "/retirarPoupanca", {
-            vcard: userStore.userPhoneNumber,
-            valor: transferAmount.value,
-        })
+  try {
+    await axios.post('vcards/' + userStore.userPhoneNumber + '/retirarPoupanca', {
+      vcard: userStore.userPhoneNumber,
+      valor: transferAmount.value
+    })
 
-        toast.success("Savings Withdrawn sucessfully!")
-        const balance = parseFloat(vcard.value.balance)
-        vcard.value.balance = parseFloat((balance + transferAmount.value).toFixed(2))
-        const savings = parseFloat(vcard.value.savings)
-        vcard.value.savings = parseFloat((savings - transferAmount.value).toFixed(2))
-    } catch (error) {
-        toast.error(error.response.data.message || "An error occurred.")
-    }
+    toast.success('Savings Withdrawn sucessfully!')
+    const balance = parseFloat(vcard.value.balance)
+    vcard.value.balance = parseFloat((balance + transferAmount.value).toFixed(2))
+    const savings = parseFloat(vcard.value.savings)
+    vcard.value.savings = parseFloat((savings - transferAmount.value).toFixed(2))
+  } catch (error) {
+    toast.error(error.response.data.message || 'An error occurred.')
+  }
 }
 
 const lastMonthTransactions = ref([])
 const loadLastMonthTransactions = () => {
-    axios
-        .get("vcard/" + userStore.userPhoneNumber + "/transactions/lastmonth", {})
-        .then((response) => {
-            lastMonthTransactions.value = response.data
-            loadChart()
-        })
-        .catch((error) => {
-            console.log(error)
-        })
+  axios
+    .get('vcard/' + userStore.userPhoneNumber + '/transactions/lastmonth', {})
+    .then((response) => {
+      lastMonthTransactions.value = response.data
+      loadChart()
+    })
+    .catch((error) => {
+      console.log(error)
+    })
 }
 
 const formatDateTime = (dateTimeString) => {
-    const [date, time] = dateTimeString.split(" ")
-    const options = { month: "short", day: "numeric" }
-    const formattedDate = new Date(date).toLocaleDateString("en-GB", options)
-    return { date: formattedDate, time: time.slice(0, 5) }
+  const [date, time] = dateTimeString.split(' ')
+  const options = { month: 'short', day: 'numeric' }
+  const formattedDate = new Date(date).toLocaleDateString('en-GB', options)
+  return { date: formattedDate, time: time.slice(0, 5) }
 }
 
 const monthNames = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December'
 ]
 
 const currentDate = new Date()
@@ -140,156 +140,152 @@ const lastMonthDate = new Date(currentDate.getFullYear(), currentDate.getMonth()
 const lastMonthMonth = lastMonthDate.getMonth() + 1
 
 const sumValues = (transactions, type) => {
-    return transactions
-        .filter((transaction) => transaction.type === type)
-        .reduce((sum, transaction) => sum + parseFloat(transaction.value), 0)
+  return transactions
+    .filter((transaction) => transaction.type === type)
+    .reduce((sum, transaction) => sum + parseFloat(transaction.value), 0)
 }
 
 const sumDebitValues = computed(() => {
-    const rawValue = sumValues(lastMonthTransactions.value, "D")
-    return parseFloat(rawValue.toFixed(2))
+  const rawValue = sumValues(lastMonthTransactions.value, 'D')
+  return parseFloat(rawValue.toFixed(2))
 })
 
 const sumCreditValues = computed(() => {
-    const rawValue = sumValues(lastMonthTransactions.value, "C")
-    return parseFloat(rawValue.toFixed(2))
+  const rawValue = sumValues(lastMonthTransactions.value, 'C')
+  return parseFloat(rawValue.toFixed(2))
 })
 
 const dateindex = ref(0)
 const dates = computed(() => {
-    const dates = lastMonthTransactions.value
-        .map((transaction) => {
-            dateindex.value += 1
-            return "(" + dateindex.value + ") " + formatDateTime(transaction.datetime).date
-        })
-        .reverse()
+  const dates = lastMonthTransactions.value
+    .map((transaction) => {
+      dateindex.value += 1
+      return '(' + dateindex.value + ') ' + formatDateTime(transaction.datetime).date
+    })
+    .reverse()
 
-    return dates
+  return dates
 })
 
 const balances = computed(() => {
-    const balances = lastMonthTransactions.value
-        .map((transaction) => {
-            return transaction.new_balance
-        })
-        .reverse()
+  const balances = lastMonthTransactions.value
+    .map((transaction) => {
+      return transaction.new_balance
+    })
+    .reverse()
 
-    return balances
+  return balances
 })
 
 const balanceChartEl = ref(null)
 let balanceChart = null
 
 const loadChart = () => {
-    if (!balanceChartEl.value) {
-        // O elemento do gráfico ainda não está disponível
-        return
-    }
+  if (!balanceChartEl.value) {
+    // O elemento do gráfico ainda não está disponível
+    return
+  }
 
-    if (balanceChart) {
-        // Destruir o gráfico anterior para evitar problemas de duplicação
-        balanceChart.destroy()
-    }
+  if (balanceChart) {
+    // Destruir o gráfico anterior para evitar problemas de duplicação
+    balanceChart.destroy()
+  }
 
-    balanceChart = new Chart(balanceChartEl.value.getContext("2d"), {
-        type: "line",
-        data: {
-            labels: dates.value,
-            datasets: [
-                {
-                    label: "Balance (€)",
-                    data: balances.value,
-                    backgroundColor: "rgba(255, 99, 132, 0.2)",
-                    borderColor: "rgba(255, 99, 132, 1)",
-                    borderWidth: 1,
-                },
-            ],
-        },
-    })
+  balanceChart = new Chart(balanceChartEl.value.getContext('2d'), {
+    type: 'line',
+    data: {
+      labels: dates.value,
+      datasets: [
+        {
+          label: 'Balance (€)',
+          data: balances.value,
+          backgroundColor: 'rgba(255, 99, 132, 0.2)',
+          borderColor: 'rgba(255, 99, 132, 1)',
+          borderWidth: 1
+        }
+      ]
+    }
+  })
 }
 
 onMounted(() => {
-    loadVCard(userStore.userPhoneNumber)
-    loadLastMonthTransactions()
+  loadVCard(userStore.userPhoneNumber)
+  loadLastMonthTransactions()
 })
 </script>
 
 <template>
-    <div class="container mt-5" v-if="userStore.userType === 'V'">
-        <div class="container mt-4 text-center">
-            <h1 class="display-4">Welcome</h1>
-            <hr class="my-4" />
-            <h2>{{ vcard.name }}</h2>
+  <div class="container mt-5" v-if="userStore.userType === 'V'">
+    <div class="container mt-4 text-center">
+      <h1 class="display-4">Welcome</h1>
+      <hr class="my-4" />
+      <h2>{{ vcard.name }}</h2>
 
-            <div class="row justify-content-center" style="margin-top: 25px">
-                <div class="col-md-3">
-                    <div
-                        class="card text-white mb-3"
-                        style="background-color: #15ba58; max-width: 18rem"
-                    >
-                        <div class="card-header text-center">Balance</div>
-                        <div class="card-body text-center">
-                            <h2 class="card-title">{{ vcard.balance }}€</h2>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-md-3">
-                    <div
-                        class="card text-white mb-3"
-                        style="background-color: #15baaa; max-width: 18rem"
-                    >
-                        <div class="card-header text-center">Savings</div>
-                        <div class="card-body text-center">
-                            <h2 class="card-title">{{ vcard.savings }}€</h2>
-                        </div>
-                    </div>
-                </div>
+      <div class="row justify-content-center" style="margin-top: 25px">
+        <div class="col-md-3">
+          <div class="card text-white mb-3" style="background-color: #15ba58; max-width: 18rem">
+            <div class="card-header text-center">Balance</div>
+            <div class="card-body text-center">
+              <h2 class="card-title">{{ vcard.balance }}€</h2>
             </div>
-            <!-- Input for transfer amount -->
-            <div class="mb-3" style="margin: 0px 40%">
-                <label for="transferAmount" class="form-label">Transfer Amount:</label>
-                <input
-                    v-model="transferAmount"
-                    type="number"
-                    class="form-control"
-                    id="transferAmount"
-                />
-            </div>
-
-            <!-- Buttons to initiate transfer -->
-            <div class="mb-3">
-                <button @click.prevent="reforcarPoupanca" class="btn btn-success me-2">
-                    Reinforce Savings
-                </button>
-                <button @click.prevent="retirarPoupanca" class="btn btn-danger">
-                    Withdraw Savings
-                </button>
-            </div>
+          </div>
         </div>
 
-        <hr />
-
-        <div class="container text-center">
-            <h3>{{ monthNames[lastMonthMonth - 1] }}</h3>
-            <h4>Your balance last month</h4>
-            <canvas
-                ref="balanceChartEl"
-                height="200px"
-                width="200px"
-                style="height: 200px; width: 200px"
-            ></canvas>
-
-            <div class="row mt-3" style="margin-bottom: 100px">
-                <div class="col-md-6">
-                    <h5>Earnings:</h5>
-                    <p class="h5 text-success">{{ sumCreditValues }}€</p>
-                </div>
-                <div class="col-md-6">
-                    <h5>Expenses:</h5>
-                    <p class="h5 text-danger">{{ sumDebitValues }}€</p>
-                </div>
+        <div class="col-md-3">
+          <div class="card text-white mb-3" style="background-color: #15baaa; max-width: 18rem">
+            <div class="card-header text-center">Savings</div>
+            <div class="card-body text-center">
+              <h2 class="card-title">{{ vcard.savings }}€</h2>
             </div>
+          </div>
         </div>
+
+        <div class="col-md-3">
+          <div class="card text-white mb-3" style="background-color: #ba1515; max-width: 18rem">
+            <div class="card-header text-center">Max Debit</div>
+            <div class="card-body text-center">
+              <h2 class="card-title">{{ vcard.max_debit }}€</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- Input for transfer amount -->
+      <div class="mb-3" style="margin: 0px 40%">
+        <label for="transferAmount" class="form-label">Transfer Amount:</label>
+        <input v-model="transferAmount" type="number" class="form-control" id="transferAmount" />
+      </div>
+
+      <!-- Buttons to initiate transfer -->
+      <div class="mb-3">
+        <button @click.prevent="reforcarPoupanca" class="btn btn-success me-2">
+          Reinforce Savings
+        </button>
+        <button @click.prevent="retirarPoupanca" class="btn btn-danger">Withdraw Savings</button>
+      </div>
     </div>
+
+    <hr />
+
+    <div class="container text-center">
+      <h3>{{ monthNames[lastMonthMonth - 1] }}</h3>
+      <h4>Your balance last month</h4>
+      <canvas
+        ref="balanceChartEl"
+        height="200px"
+        width="200px"
+        style="height: 200px; width: 200px"
+      ></canvas>
+
+      <div class="row mt-3" style="margin-bottom: 100px">
+        <div class="col-md-6">
+          <h5>Earnings:</h5>
+          <p class="h5 text-success">{{ sumCreditValues }}€</p>
+        </div>
+        <div class="col-md-6">
+          <h5>Expenses:</h5>
+          <p class="h5 text-danger">{{ sumDebitValues }}€</p>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
